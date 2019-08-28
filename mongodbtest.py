b0d16d9ac51042245c62f08ba184238101ec0cf0
Furn Techalertumpai <furncyn@ucla.edu>
2019-08-27 23:10:03 -0700

    Setting up database

--- mongodbtest.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-# Establish connection with PyMongo
-# <<MongoDB URL>> is a placeholder for the connection string to MongoDB
-client = MongoClient('<<MongoDB URL>>')
-db = client.business 
