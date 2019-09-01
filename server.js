d80fc4a57ce4394cab4976e7320e91f46593dbac
Furn Techalertumpai <furncyn@ucla.edu>
2019-08-31 17:48:52 -0700

    Added a simple web page that is connected to mongodb.

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const express = require('express')
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
-const db = require('./config/db');
 const app = express();
-const port = 8000;
+const port = 3000;
+
+const url = "mongodb+srv://furncyn:courtside5@watchlist-4bthd.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
-MongoClient.connect(db.url, (err,database) =>{
-    if (err) return console.log(err)
-        require('./app/routes')(app, database);
-    app.listen(port, () => {
-        console.log("We are live on"+port);
-    });
+// Connect to database and create collection
+MongoClient.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.log(err);
+    var db = client.db("customers_watchlist");
+    
+    app.listen(port, function() {
+        console.log(`listening on port ${port}`);
+    })
+
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray((err, result) => {
+            if (err) return console.log(err)
+            // renders index.ejs
+            res.render('index.ejs', {quotes: result})
+          })
+    })
+
+    app.post('/quotes', (req, res) => {
+        db.collection('quotes').save(req.body, (err, result) => {
+            if (err) return console.log(err)
+            console.log('saved to database')
+            res.redirect('/')
+        })
+    })
 });
